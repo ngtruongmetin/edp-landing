094483dba34468761b70676a27e82dc60eb630aa
Ngoc Truong Dang <ngtruongmetin@gmail.com>
2026-07-01 23:37:45 +0700

    Update contact email address

--- src/config/contact.js
+++ src/config/contact.js
@@ -1,4 +1,4 @@
-export const CONTACT_EMAIL = 'edp@gmail.com'
+export const CONTACT_EMAIL = 'edpstartup26@gmail.com'
 export const CONTACT_PHONE = '+84865916475'
 
 export const CONTACT_EMAIL_HREF = `mailto:${CONTACT_EMAIL}`
